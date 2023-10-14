8d2c191ba56224027f67a775fe41ac4de4d72334
Simonaytes <yandexman228@gmail.com>
2023-10-14 10:43:27 +0300

    Add icon for brush

--- Src/Resources.h
+++ Src/Resources.h
@@ -5,6 +5,8 @@ static const char* kBackgroundImgFile        = "Resources/SimpleButton.png";
 static const char* kBackgroundPressedImgFile = "Resources/SimpleButtonPressed.png";
 static const char* kCloseImgFile             = "Resources/Close.png";
 static const char* kClosePressedImgFile      = "Resources/ClosePressed.png";
+static const char* kBrushImgFile             = "Resources/Brush.png";
+static const char* kBrushPressedImgFile      = "Resources/BrushPressed.png";
 static const char* kFontFile                 = "Resources/Font.ttf";
 
 #endif //SYM_RESOURCES
