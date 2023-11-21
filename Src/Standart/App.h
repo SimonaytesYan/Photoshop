111d1806899548ed2d33028156838f42c47605d2
Simonaytes <yandexman228@gmail.com>
2023-11-21 21:20:00 +0300

    Add struct App

---
+++ Src/Standart/App.h
@@ -0,0 +1,19 @@
+#ifndef SYM_APP
+#define SYM_APP
+
+#include "GuiI.h"
+#include "EventManagerI.h"
+#include "ToolI.h"
+#include "FilterI.h"
+
+namespace plugin
+{
+    struct App 
+    {
+        GuiI *root;
+        EventManagerI *event_manager; 
+        ToolManagerI *tool_manager;
+        FilterManagerI *filter_manager; 
+    };
+}
+#endif //SYM_APP
