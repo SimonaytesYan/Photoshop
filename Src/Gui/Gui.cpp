f1e5a5bbe62bc7eca6f9c6b11c29731ac5baeb36
Simonaytes <yandexman228@gmail.com>
2023-11-21 20:51:26 +0300

    Add GuiI and Gui classes

---
+++ Src/Gui/Gui.cpp
@@ -0,0 +1,17 @@
+#include "Gui.h"
+
+plugin::RenderTargetI* Gui::getRenderTarget(plugin::Vec2    size, 
+                                            plugin::Vec2    pos, 
+                                            plugin::Plugin *self)
+{
+
+    //Widget* 
+    //root->registerSubWidget();
+}
+
+void Gui::createParamWindow(plugin::Array<const char*> param_names, 
+                            plugin::Interface*         self)
+{
+
+}
+
---
+++ Src/Standart/GuiI.h
@@ -0,0 +1,38 @@
+#ifndef SYM_GUI_I
+#define SYM_GUI_I
+
+#include "../Vec2/Vec2.h"
+#include "RenderTargetI.h"
+#include "WidgetI.h"
+#include "../Useful.h"
+#include "Plugin.h"
+#include "Interface.h" 
+
+namespace plugin
+{
+    struct GuiI 
+    {
+        virtual plugin::Vec2 getSize() = 0; // размер доступной для рисования площади (которую можно запросить)
+
+        /// @brief запросить RT.
+        /// Хост создает новое окно / отдает какое-то, абсолютно пустое, с единственным RT на все окно.
+        /// @param size -- размер запрашиваемой области
+        /// @param pos  -- (относительное [относительно предоставленной области]) смещение запрашиваемой области
+        virtual RenderTargetI* getRenderTarget(plugin::Vec2 size, plugin::Vec2 pos, Plugin *self) = 0;
+
+        /// @brief Создает окно с параметрами, каким-то образом узнает у пользователя 
+        ///     значения параметров и потом возвращает их интерфейсу через Interface::set_params
+        /// @note окно не обязательно модальное, да и вообще implementation defined. Мем в том, что плагин находится в 
+        ///     неопределенном/дефолтном состоянии между createParamWindow и Interface::set_params и взаимодействие с ним UB
+        virtual void createParamWindow(Array<const char *> param_names, Interface * self) = 0;
+
+        /**
+         * @brief Get the root widget of widget tree
+         * 
+         * @return WidgetI* root
+         */
+        virtual WidgetI* getRoot();
+    };
+} 
+
+#endif //SYM_GUI_I
